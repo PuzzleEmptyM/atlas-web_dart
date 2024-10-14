951be9e88de613a99c9d01dee5ba27fe6b510758
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-10-14 14:43:23 -0500

    password validation

---
+++ 0x02-dart-classes/2-main.dart
@@ -0,0 +1,21 @@
+import '2-password.dart';
+
+void main() {
+  final ps = Password();
+
+  ps.password = "Just3z";
+  if (ps.isValid()) {
+    print("${ps.password} is a Valid Password");
+  } else {
+    print("${ps.password} is Not a Valid Password");
+  }
+  print(ps.toString());
+
+  ps.password = "ShouldWorkf7ne";
+  if (ps.isValid()) {
+    print("${ps.password} is a Valid Password");
+  } else {
+    print("${ps.password} is Not a Valid Password");
+  }
+  print(ps.toString());
+}
