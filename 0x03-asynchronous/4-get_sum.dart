715993b761570e8d9f5480b73a1d965685d9c2ad
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-10-16 20:17:56 -0500

    updated it to ensure that both int and double values are handled correctly

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -3,23 +3,32 @@ import '4-util.dart';
 
 Future<double> calculateTotal() async {
   try {
+    print('Fetching user data...');
     String userData = await fetchUserData();
     Map<String, dynamic> userMap = jsonDecode(userData);
     String userId = userMap['id'];
+    print('User ID: $userId');
 
+    print('Fetching user orders...');
     String ordersData = await fetchUserOrders(userId);
     List<dynamic> orders = jsonDecode(ordersData);
+    print('User Orders: $orders');
 
     double totalPrice = 0.0;
 
     for (var product in orders) {
+      print('Fetching price for product: $product');
       String priceData = await fetchProductPrice(product);
-      double productPrice = jsonDecode(priceData);
-      totalPrice += productPrice;
+      var productPrice = jsonDecode(priceData);
+
+      totalPrice += (productPrice is int) ? productPrice.toDouble() : productPrice;
+      print('Price for $product: $productPrice');
     }
 
+    print('Total Price: $totalPrice');
     return totalPrice;
   } catch (error) {
+    print('Error caught: $error');
     return -1;
   }
 }
